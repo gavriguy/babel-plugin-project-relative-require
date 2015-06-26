a8e08e8dac98b4f1130c1079b535173149bb0ef8
Gavri (Gabriel) Guy <gavri.guy@gmail.com>
2015-06-26 16:34:05 +0300

    first-commit

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = function (babel) {
+  // get the working directory
+  var cwd = process.cwd();
+
+  return new babel.Transformer("babel-plugin-project-relative-require", {
+    ImportDeclaration: function(node, parent) {
+      // probably always true, but let's be safe
+      if (!babel.types.isLiteral(node.source)) {
+        return node;
+      }
+
+      var ref = node.source.value;
+
+      // ensure a value, make sure it's not home relative e.g. ~/foo
+      if (!ref || ref[0] !== '~' || ref[1] === '/') {
+        return node;
+      }
+
+      node.source.value = cwd + '/' + node.source.value.slice(1);
+
+      return node;
+    }
+  });
+};
+
+
